8b64246226264d40265c70e880009d2f6d763d2f
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-07-10 10:50:40 +0600

    remove dist and node folder from github

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from 'express';
 
-app.get("/", (req, res) =>  {
+const app = express();
+
+
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port = 5000;
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
