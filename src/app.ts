c1d01337e12aa078f4749345b8ed301eb9c13403
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-09-04 02:34:14 +0600

    add frontend live url in the cors origin

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,17 @@ export const stripe = require("stripe")(config.payment_secret_key);
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+const corsOptions = {
+  credentials: true,
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://flora-fusion-backend.vercel.app/",
+    "https://flora-fussing.vercel.app/",
+  ],
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/v1/", router);
 
